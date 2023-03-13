2d1268592fbceb6bcf3d106db3b32f6e61c9e2a6
coffinslugs <55113474+coffinslugs@users.noreply.github.com>
2023-03-13 14:06:02 -0600

    Add project files.

---
+++ HotelAppDB/dbo/Tables/RoomType.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[RoomType]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Title] NVARCHAR(50) NOT NULL, 
+    [Description] NVARCHAR(2000) NOT NULL, 
+    [Price] MONEY NOT NULL
+)
