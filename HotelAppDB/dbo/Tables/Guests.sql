94fe0dd40f72baddfa77bf75793bf2f2bb50a1cf
coffinslugs <55113474+coffinslugs@users.noreply.github.com>
2023-03-20 09:08:57 -0600

    Setting Identity on Guests Id column

--- HotelAppDB/dbo/Tables/Guests.sql
+++ HotelAppDB/dbo/Tables/Guests.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[Guests]
 (
-	[Id] INT NOT NULL PRIMARY KEY, 
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
     [FirstName] NVARCHAR(50) NOT NULL, 
     [LastName] NVARCHAR(50) NOT NULL
 )
