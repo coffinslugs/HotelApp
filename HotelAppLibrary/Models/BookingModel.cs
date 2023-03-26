c27f265c2ba24d9bbf7543e9ba3303395ab7b9bc
coffinslugs <55113474+coffinslugs@users.noreply.github.com>
2023-03-25 20:21:24 -0600

    Models created

---
+++ HotelAppLibrary/Models/BookingModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HotelAppLibrary.Models
+{
+    public class BookingModel
+    {
+        public int Id { get; set; }
+        public int RoomId { get; set; }
+        public int GuestId { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public bool CheckedIn { get; set; }
+        public float TotalCost { get; set; }
+    }
+}
