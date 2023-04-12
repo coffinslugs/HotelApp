9a947ac03cc3371214e1fdad5b01e1b464c3e341
coffinslugs <55113474+coffinslugs@users.noreply.github.com>
2023-04-11 20:44:41 -0600

    new stored proc to check in a guest.

---
+++ HotelAppDB/dbo/Stored Procedures/spBookings_CheckIn.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE [dbo].[spBookings_CheckIn]
+	@Id int
+AS
+BEGIN 
+	SET NOCOUNT ON;
+
+	UPDATE dbo.Bookings
+	SET CheckedIn = 1
+	WHERE Id = @Id;
+
+END
--- HotelAppLibrary/Data/SqlData.cs
+++ HotelAppLibrary/Data/SqlData.cs
@@ -68,5 +68,11 @@ namespace HotelAppLibrary.Data
                                                                         true);
         }
 
+        public void CheckInGuest(int bookingId)
+        {
+            
+            _db.SaveData("dbo.spBookings_CheckIn", new { Id = bookingId }, connectionStringName,true);
+        }
+
     }
 }
