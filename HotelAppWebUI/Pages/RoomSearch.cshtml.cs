8896a482c1000e749dc97e3c0981ca4cbef5d3db
coffinslugs <55113474+coffinslugs@users.noreply.github.com>
2023-04-27 10:03:58 -0600

    LocalDB connection string added Search calls GetAvailableRoomTypes

--- HotelAppWebUI/Pages/RoomSearch.cshtml.cs
+++ HotelAppWebUI/Pages/RoomSearch.cshtml.cs
@@ -1,27 +1,48 @@
+using HotelAppLibrary.Data;
+using HotelAppLibrary.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace HotelAppWebUI.Pages
 {
     public class RoomSearchModel : PageModel
     {
+        private readonly IDatabaseData _db;
+
         [DataType(DataType.Date)]
-        [BindProperty]
+        [BindProperty(SupportsGet = true)]
         public DateTime StartDate { get; set; } = DateTime.Now;
 
         [DataType(DataType.Date)]
-        [BindProperty]
+        [BindProperty(SupportsGet = true)]
         public DateTime EndDate { get; set; } = DateTime.Now.AddDays(1);
+
+        [BindProperty(SupportsGet = true)]
+        public bool SearchEnabled { get; set; } = false;
+
+        public List<RoomTypeModel> AvailableRoomTypes { get; set; }
+
+        public RoomSearchModel(IDatabaseData db)
+        {
+            _db = db;
+        }
 
         public void OnGet()
         {
+            if (SearchEnabled == true)
+            {
+                AvailableRoomTypes = _db.GetAvailableRoomTypes(StartDate, EndDate);
+            }
+
         }
 
         public IActionResult OnPost()
         {
-            return Page();
+
+            return RedirectToPage(new { SearchEnabled = true, StartDate, EndDate });
         }
     }
 }
